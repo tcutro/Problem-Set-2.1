117622c21685342ebb6880c549e70871341275de
Thomas Cutro <tcutro@ucvts.tec.nj.us>
2020-01-31 13:04:43 -0500

    first commit

---
+++ ProblemSet2_1.java
@@ -0,0 +1,23 @@
+package problemset2_1;
+
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        System.out.println("  XXXX  ");
+        System.out.println(" X    X ");
+        System.out.println("X X  X X");
+        System.out.println("X      X");
+        System.out.println("X X  X X");
+        System.out.println("X  XX  X");
+        System.out.println(" X    X ");
+        System.out.println("  XXXX  ");
+        
+        
+        
+        
+        
+        
+        
+    }   
+}
